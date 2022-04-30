bc90f3157e62c8f9cfe07b14c58a7c86bb968f90
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-05-01 00:37:18 +0900

    入力内容をstateで管理する

--- src/components/task/TaskCard.tsx
+++ src/components/task/TaskCard.tsx
@@ -1,17 +1,24 @@
 import { Card, CardContent } from "@mui/material";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import TaskCardDeleteButton from "./button/TaskCardDeleteButton";
 import TaskCardAddInput from "./input/TaskCardAddInput";
 import TaskCardTitle from "./TaskCardTitle";
 import TasksArea from "./TasksArea";
 
 const TaskCard: FC = () => {
+  const [inputText, setInputText] = useState("");
+  const [taskList, setTaskList] = useState<string[]>([]);
   return (
     <Card variant="outlined" sx={{ width: { xs: "100%", md: 250 } }}>
       <CardContent>
         <TaskCardTitle />
         <TaskCardDeleteButton />
-        <TaskCardAddInput />
+        <TaskCardAddInput
+          inputText={inputText}
+          setInputText={setInputText}
+          taskList={taskList}
+          setTaskList={setTaskList}
+        />
         <TasksArea />
       </CardContent>
     </Card>
--- src/components/task/input/TaskCardAddInput.tsx
+++ src/components/task/input/TaskCardAddInput.tsx
@@ -1,7 +1,12 @@
 import { FormControl, TextField } from "@mui/material";
-import React, { FC } from "react";
-
-const TaskCardAddInput: FC = () => {
+import React, { Dispatch, FC, SetStateAction } from "react";
+type Props = {
+  inputText: string;
+  setInputText: Dispatch<SetStateAction<string>>;
+  taskList: string[];
+  setTaskList: Dispatch<SetStateAction<string[]>>;
+};
+const TaskCardAddInput: FC<Props> = (props) => {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault;
     console.log(e);
